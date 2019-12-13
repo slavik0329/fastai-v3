ee04092922433481d9269bf27a98bc68bbf943d6
Steve Dakh <slavik0329@gmail.com>
2019-12-13 12:39:12 -0500

    more

--- app/server.py
+++ app/server.py
@@ -70,8 +70,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': lesion_type_dict[str(prediction)]})
+    prediction,idx,outputs = learn.predict(img)
+    return JSONResponse({'result': lesion_type_dict[str(prediction)], "probs":outputs.tolist()})
 
 
 if __name__ == '__main__':
